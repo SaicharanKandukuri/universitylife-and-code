82c3a4e6e9ea70cfcacb866a93f2681f4e57fe8a
theaarzoo <aarzooalam11@gmail.com>
2021-03-24 20:31:05 +0530

    Compare 3 Numbers for largest

--- classes/lab/comapre3num.c
+++ classes/lab/comapre3num.c
@@ -1,12 +1,15 @@
+/* Q- Write a programm to comapre 3 numbers by user input & print the largest number
+ by- github.com/theaarzoo */
+
 #include <stdio.h>
 int main() {
     double n1, n2, n3;
     printf("Enter three numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    scanf("%lf %lf %lf", &n1, &n2, &n3); //lf : long float
 
     if (n1 >= n2) {
         if (n1 >= n3)
-            printf("%.2lf is the largest number.", n1);
+            printf("%.2lf is the largest number.", n1); //.2lf: It'll print decimal values to 2 places only
         else
             printf("%.2lf is the largest number.", n3);
     } else {
