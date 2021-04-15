4784ae4fd270b3ae3d6afae01fd88e7b866ba43d
Zman-1x1 <68287637+SaicharanKandukuri@users.noreply.github.com>
2021-04-15 10:10:09 +0530

    Create number_to_days.c

---
+++ classes/lab/number_to_days.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void main(void) {
+    int days;
+
+    printf("please enter day number \n : ");
+    scanf("%d",&days);
+
+    if (days <= 7 )
+    {
+        if ( days == 1) {
+            printf("Sunday");
+        }
+        else if (days == 2)
+        {
+            printf("Monday");
+        }
+        else if (days == 3)
+        {
+            printf("Tuesday");
+        }
+        else if (days == 4)
+        {
+            printf("Wednesay");
+        }
+        else if (days == 5)
+        {
+            printf("Thursday");
+        }
+        else if (days == 6)
+        {
+            printf("friday");
+        }
+        else if (days == 7)
+        {
+            printf("Saturday");
+        }
+
+    }
+    else printf("The Given input \"%d\" is invalid please enter number between 1- 7 ");
+    main();
+    
+}
